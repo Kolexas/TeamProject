eaa27702435deb93226f3c1a3ca2d2c8e3bd2954
Kolexas <maksimilyan.klimentev39@gmail.ru>
2025-06-18 15:31:49 +0300

    Added first set of rules

--- src/main/java/com/starbank/teamproject/repository/RecommendationRuleInvest500.java
+++ src/main/java/com/starbank/teamproject/repository/RecommendationRuleInvest500.java
@@ -8,8 +8,25 @@ import java.util.UUID;
 
 @Component
 public class RecommendationRuleInvest500 implements RecommendationRuleSet {
+
+    private RecommendationsRepository recommendationsRepository;
+
+    public RecommendationRuleInvest500(RecommendationsRepository recommendationsRepository) {
+        this.recommendationsRepository = recommendationsRepository;
+    }
+
     @Override
     public Optional<RecommendationDTO> CheckRule(UUID userId) {
+        boolean First_Rule = recommendationsRepository.CheckProductExistencesByType(userId, "DEBIT");
+        boolean Second_Rule = recommendationsRepository.CheckProductExistencesByType(userId, "INVEST");
+        boolean Third_Rule = recommendationsRepository.CheckSumTransactionByTransactionTypeAndByProductType(userId, "DEPOSIT", "SAVING") > 1_000;
+
+
+        if (First_Rule && !Second_Rule && Third_Rule) {
+            return Optional.of(new RecommendationDTO(userId, "Invest 500", "Откройте свой путь к успеху с индивидуальным инвестиционным счетом (ИИС) от нашего банка! " +
+                    "Воспользуйтесь налоговыми льготами и начните инвестировать с умом. Пополните счет до конца года и получите выгоду в виде вычета на взнос в следующем налоговом " +
+                    "периоде. Не упустите возможность разнообразить свой портфель, снизить риски и следить за актуальными рыночными тенденциями. Откройте ИИС сегодня и станьте ближе к финансовой независимости!"));
+        }
         return Optional.empty();
     }
 }
